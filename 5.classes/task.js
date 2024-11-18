d211dd946cafb0cf110340686f5e740a41c055e9
Svetlana neto <khodsvetlana@yandex.ru>
2024-11-18 18:38:20 +0200

    Пятое задание готово

---
+++ 5.classes/task.js
@@ -0,0 +1,180 @@
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+
+	}
+	fix() {
+		this.state *= 1.5;
+	}
+
+	set state(newState) {
+		if (newState < 0) {
+			this._state = 0;
+		} else if (newState > 100) {
+			this._state = 100;
+		} else {
+			this._state = newState;
+		}
+	}
+	get state() {
+		return this._state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.author = author;
+		this.type = "book";
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "fantastic";
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "detective";
+	}
+}
+
+//задача 2
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+	findBookBy(key, value) {
+		for (let book of this.books) {
+			if (book[key] === value) {
+				return book;
+			}
+		}
+		return null;
+	}
+	giveBookByName(bookName) {
+		for (let book of this.books) {
+			if (book.name === bookName) {
+				return this.books.splice(this.books.indexOf(book), 1)[0];
+			}
+		}
+		return null;
+	}
+}
+
+const sherlock = new PrintEditionItem(
+	"Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+	2019,
+	1008
+);
+console.log(sherlock.releaseDate); //2019
+console.log(sherlock.state); //100
+sherlock.fix();
+console.log(sherlock.state); //100
+
+const picknick = new FantasticBook(
+	"Аркадий и Борис Стругацкие",
+	"Пикник на обочине",
+	1972,
+	168
+);
+console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+picknick.state = 10;
+console.log(picknick.state); //10
+picknick.fix();
+console.log(picknick.state); //15
+
+const library = new Library("Библиотека имени Ленина");
+library.addBook(
+	new DetectiveBook(
+		"Артур Конан Дойл",
+		"Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+		2019,
+		1008
+	)
+);
+library.addBook(
+	new FantasticBook(
+		"Аркадий и Борис Стругацкие",
+		"Пикник на обочине",
+		1972,
+		168
+	)
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг
+
+//Задача 3
+class Student {
+	constructor(name) {
+		this.name = name;
+		this.marks = {};
+	}
+	addMark(mark, subject) {
+		if (mark >= 2 && mark <= 5) {
+			if (!this.marks[subject]) {
+				this.marks[subject] = [];
+			}
+			this.marks[subject].push(mark);
+		} else {
+			console.log(`Некорректная оценка`);
+		}
+	}
+	getAverageBySubject(subject) {
+		if (this.marks[subject]) {
+			return this.marks[subject].reduce((sum, mark) => sum + mark, 0) / this.marks[subject].length;
+		} else {
+			return 0;
+		}
+	}
+	getAverage() {
+		if (Object.keys(this.marks).length > 0) {
+			return Object.keys(this.marks).reduce((sum, subject) => sum + this.getAverageBySubject(subject), 0) / Object.keys(this.marks).length;
+		} else {
+			return 0;
+		}
+	}
+}
+
+const student = new Student("Олег Никифоров");
+student.addMark(5, "химия");
+student.addMark(5, "химия");
+student.addMark(5, "физика");
+student.addMark(4, "физика");
+student.addMark(6, "физика"); // Оценка не добавится, так как больше 5
+student.getAverageBySubject("физика"); // Средний балл по предмету физика 4.5
+student.getAverageBySubject("биология"); // Вернёт 0, так как по такому предмету нет никаких оценок.
+student.getAverage(); // Средний балл по всем предметам 4.75
