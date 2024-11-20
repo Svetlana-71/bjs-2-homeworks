b5cc9583aca8aacfcfc4fbb55656b968f607f9cf
Svetlana neto <khodsvetlana@yandex.ru>
2024-11-20 12:42:07 +0200

    Шестое задание частично готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value); 
+    if (isNaN(parsedValue)) {
+      throw new Error('Невалидное значение');
+    }  
+    return parsedValue;
+}
+  
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC; 
+    }
+    get perimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+    get area() {
+        const halfPerimeter = this.perimeter / 2;
+        const area = Math.sqrt(halfPerimeter * (halfPerimeter - this.sideA) * (halfPerimeter - this.sideB) * (halfPerimeter - this.sideC));
+        return validateCount(area.toFixed(3));
+    }
+}
+function getTriangle(sideA, sideB, sideC) {
+    try {
+        return new Triangle(sideA, sideB, sideC);
+    } catch (error) {
+        return {area: 'Ошибка! Треугольник не существует', perimeter: 'Ошибка! Треугольник не существует'};       
+    }
+        
+}
+  
+
+
+
+
+
+    
+
+
